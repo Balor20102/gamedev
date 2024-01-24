1b92771f18f38a845545e73d2d451c205a833c1a
DanielKool <daniel@familiekool.nl>
2024-01-24 11:53:24 +0100

    blocks spawn above the player and offset

--- playerCharacter.gd
+++ playerCharacter.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 150.0
-const JUMP_VELOCITY = -250.0
+const JUMP_VELOCITY = -500.0
 
 @onready var sprite_2d = $AnimatedSprite2D
 
--- scripts/TileMap.gd
+++ scripts/TileMap.gd
@@ -2,14 +2,14 @@ extends TileMap
 
 #tetrominoes
 var i_0 := [Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(3, 1)]
-var i_90 := [Vector2i(2, 0), Vector2i(2, 1), Vector2i(2, 2), Vector2i(2, 3)]
-var i_180 := [Vector2i(0, 2), Vector2i(1, 2), Vector2i(2, 2), Vector2i(3, 2)]
-var i_270 := [Vector2i(1, 0), Vector2i(1, 1), Vector2i(1, 2), Vector2i(1, 3)]
+var i_90 := [Vector2i(0, 0), Vector2i(0, 1), Vector2i(0, 2), Vector2i(0, 3)]
+var i_180 := [Vector2i(3, 1), Vector2i(2, 1), Vector2i(1, 1), Vector2i(0, 1)]
+var i_270 := [Vector2i(0, 3), Vector2i(0, 2), Vector2i(0, 1), Vector2i(0, 0)]
 var i := [i_0, i_90, i_180, i_270]
 
-var t_0 := [Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1)]
-var t_90 := [Vector2i(1, 0), Vector2i(1, 1), Vector2i(2, 1), Vector2i(1, 2)]
-var t_180 := [Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(1, 2)]
+var t_0 := [Vector2i(0, 1), Vector2i(-1, 2), Vector2i(-0, 2), Vector2i(1, 2)]
+var t_90 := [Vector2i(0, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(0, 2)]
+var t_180 := [Vector2i(1, 2), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1)]
 var t_270 := [Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(1, 2)]
 var t := [t_0, t_90, t_180, t_270]
 
@@ -19,26 +19,26 @@ var o_180 := [Vector2i(0, 0), Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1)]
 var o_270 := [Vector2i(0, 0), Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1)]
 var o := [o_0, o_90, o_180, o_270]
 
-var z_0 := [Vector2i(0, 0), Vector2i(1, 0), Vector2i(1, 1), Vector2i(2, 1)]
-var z_90 := [Vector2i(2, 0), Vector2i(1, 1), Vector2i(2, 1), Vector2i(1, 2)]
+var z_0 := [Vector2i(0, 1), Vector2i(1, 1), Vector2i(1, 2), Vector2i(2, 2)]
+var z_90 := [Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(0, 2)]
 var z_180 := [Vector2i(0, 1), Vector2i(1, 1), Vector2i(1, 2), Vector2i(2, 2)]
 var z_270 := [Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(0, 2)]
 var z := [z_0, z_90, z_180, z_270]
 
-var s_0 := [Vector2i(1, 0), Vector2i(2, 0), Vector2i(0, 1), Vector2i(1, 1)]
-var s_90 := [Vector2i(1, 0), Vector2i(1, 1), Vector2i(2, 1), Vector2i(2, 2)]
-var s_180 := [Vector2i(1, 1), Vector2i(2, 1), Vector2i(0, 2), Vector2i(1, 2)]
+var s_0 := [Vector2i(0, 1), Vector2i(1, 1), Vector2i(-1, 2), Vector2i(0, 2)]
+var s_90 := [Vector2i(0, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(1, 2)]
+var s_180 := [Vector2i(0, 1), Vector2i(1, 1), Vector2i(-1, 2), Vector2i(0, 2)]
 var s_270 := [Vector2i(0, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(1, 2)]
 var s := [s_0, s_90, s_180, s_270]
 
-var l_0 := [Vector2i(2, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1)]
-var l_90 := [Vector2i(1, 0), Vector2i(1, 1), Vector2i(1, 2), Vector2i(2, 2)]
+var l_0 := [Vector2i(2, 1), Vector2i(0, 2), Vector2i(1, 2), Vector2i(2, 2)]
+var l_90 := [Vector2i(0, 0), Vector2i(0, 1), Vector2i(0, 2), Vector2i(1, 2)]
 var l_180 := [Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(0, 2)]
 var l_270 := [Vector2i(0, 0), Vector2i(1, 0), Vector2i(1, 1), Vector2i(1, 2)]
 var l := [l_0, l_90, l_180, l_270]
 
-var j_0 := [Vector2i(0, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1)]
-var j_90 := [Vector2i(1, 0), Vector2i(2, 0), Vector2i(1, 1), Vector2i(1, 2)]
+var j_0 := [Vector2i(0, 1), Vector2i(0, 2), Vector2i(1, 2), Vector2i(2, 2)]
+var j_90 := [Vector2i(0, 0), Vector2i(1, 0), Vector2i(0, 1), Vector2i(0, 2)]
 var j_180 := [Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(2, 2)]
 var j_270 := [Vector2i(1, 0), Vector2i(1, 1), Vector2i(0, 2), Vector2i(1, 2)]
 var j := [j_0, j_90, j_180, j_270]
@@ -53,15 +53,17 @@ const ROWS : int = 20
 const directions := [Vector2i.LEFT, Vector2i.RIGHT, Vector2i.DOWN]
 var steps : Array
 const steps_req : int = 50
-const start_pos := Vector2i(5,1)
+const start_pos := Vector2i(0,1)
 var cur_pos : Vector2i
 var speed : float
 const ACCEL : float = 0.25
 
 #game piece variables
+var player_cor_x
 var piece_type
 var next_piece_type
 var rotation_index : int = 0
+var next_rotation_index : int = 0
 var active_piece : Array
 
 #game variables
@@ -83,14 +85,14 @@ func _ready():
 	new_game()
 	$HUD.get_node("StartButton").pressed.connect(new_game)
 	
-	
 func new_game():
 	#reset variabels
 	$HUD.get_node("StartButton").release_focus()
 	score = 0
-	speed = 1.0
+	speed = 3.0
 	game_running = true
 	steps = [0, 0, 0] #0:left, 1:right, 2:down
+	rotation_index = randf_range(1,4)
 	$HUD.get_node("GameOverLabel").hide()
 	#clear everything
 	clear_piece()
@@ -103,20 +105,10 @@ func new_game():
 	next_piece_atlas = Vector2i(shapes_full.find(next_piece_type), 0)
 	create_piece()
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 #apply downward movement every frame
 	if game_running:
-		if Input.is_action_pressed("ui_left"):
-			steps[0] += 10
-		elif Input.is_action_pressed("ui_right"):
-			steps[1] += 10
-		elif Input.is_action_pressed("ui_down"):
-			steps[2] += 10
-		elif Input.is_action_just_pressed("ui_up"):
-			rotate_piece()
-
 		steps[2] += speed
 		#move the piece
 		for i in range(steps.size()):
@@ -138,11 +130,25 @@ func pick_piece():
 func create_piece():
 	#reset variables
 	steps = [0, 0, 0] #0:left, 1:right, 2:down
-	cur_pos = start_pos
+	player_cor_x = local_to_map($CharacterBody2D.position).x
+	
 	active_piece = piece_type[rotation_index]
+	
+	var off_set = spawn_off_set(rotation_index)
+	
+	cur_pos = Vector2i(player_cor_x, start_pos.y)
+
+	
+	if (player_cor_x > 8 - off_set and active_piece == i[0] or player_cor_x > 8 - off_set and active_piece == i[2] ):
+		cur_pos.x =  8 - off_set
+	elif (player_cor_x > 10 - off_set ):
+		cur_pos.x =  10 - off_set
+	elif (player_cor_x < 1 + off_set):
+		cur_pos.x = 0 + off_set
+		
 	draw_piece(active_piece, cur_pos, piece_atlas)
 	#show next piece
-	draw_piece(next_piece_type[0], Vector2i(15,6), next_piece_atlas)
+	draw_piece(next_piece_type[next_rotation_index], Vector2i(15,6), next_piece_atlas)
 	
 func clear_piece():
 	for i in active_piece:
@@ -151,13 +157,6 @@ func clear_piece():
 func draw_piece(piece, pos, atlas):
 	for i in piece:
 		set_cell(active_layer, pos + i, tile_id, atlas)
-
-func rotate_piece():
-	if can_rotate():
-		clear_piece()
-		rotation_index = (rotation_index + 1) % 4
-		active_piece = piece_type[rotation_index]
-		draw_piece(active_piece, cur_pos, piece_atlas)
 	
 func move_piece(dir):
 	if can_move(dir):
@@ -170,8 +169,10 @@ func move_piece(dir):
 			check_rows()
 			piece_type = next_piece_type
 			piece_atlas = next_piece_atlas
+			rotation_index = next_rotation_index
 			next_piece_type = pick_piece()
 			next_piece_atlas = Vector2i(shapes_full.find(next_piece_type),0)
+			next_rotation_index = randf_range(1,4)
 			clear_panel()
 			create_piece()
 			check_game_over()
@@ -183,15 +184,7 @@ func can_move(dir):
 		if not is_free(i + cur_pos + dir):
 			cm = false
 	return cm
-	
-func can_rotate(): 
-	var cr = true
-	var temp_rotation_index =  (rotation_index + 1) % 4
-	for i in piece_type[temp_rotation_index]:
-		if not is_free(i + cur_pos):
-			cr = false
-	return cr
-	
+
 func is_free(pos):
 	return get_cell_source_id(board_layer, pos) == -1
 
@@ -202,8 +195,8 @@ func land_piece():
 		set_cell(board_layer, cur_pos + i, tile_id, piece_atlas)
 
 func clear_panel():
-	for i in range(14,19):
-		for j in range(5,9):
+	for i in range(15,20):
+		for j in range(6,10):
 			erase_cell(active_layer, Vector2i(i,j))
 
 func check_rows():
@@ -237,10 +230,26 @@ func clear_board():
 		for j in range(COLS):
 			erase_cell(board_layer, Vector2i(j+1, i+1))
 
-
 func check_game_over():
 	for i in active_piece:
 		if not is_free(i+ cur_pos):
 			land_piece()
 			$HUD.get_node("GameOverLabel").show()
 			game_running = false
+
+func spawn_off_set(rotation):
+	var off_set = 0
+	if (rotation == 1 or rotation == 3):
+		for i in active_piece:
+			if (off_set < i.x):
+				off_set = i.x
+	if (rotation == 0 or rotation == 2):
+		for i in active_piece:
+			if (off_set < i.y):
+				off_set = i.y
+	return off_set
+	
+	
+	
+	
+
